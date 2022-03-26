1dd38d32fae73c6b9309a0d6b1951dbd2f19d595
Samir Zade <101379923+samirzade001@users.noreply.github.com>
2022-03-26 08:06:19 -0700

    Add files via upload

--- Mini - Quiz game project.py
+++ Mini - Quiz game project.py
@@ -1,55 +1,56 @@
-# Samir zade 
-# @samirzade001
+#samir zade
+#samirzade001
 
-print("(--------------- Welcome to computer quiz!!----------------)")
-User_asking = input("Do you want to play? ")
-
-if User_asking.lower() != "yes":
-    print("Ok, thank you for playing :)")
+name = input("Enter your name player: ")  #Taking user name as player
+print(f"(--------------- Welcome {name} to computer quiz!!----------------)")
+User_asking = input(f"{name}, Do you want to play? ")  #Asking user for it's playing intrest or not
+ 
+if User_asking.lower() != "yes":  #if user not intrested in playing
+    print("Ok, thank you!! :)")  
     quit()
 
-print("Let's start :) ")
-score = 0
-answer = input("121 divided by 11 is ")
+print("Let's start :) ")  
+score = 0  #storing the score of user for showing it's score of wining
+answer = input("\n121 divided by 11 is ")
 if answer == '11' or answer.lower() == "eleven":
-    print("Correct answer :)")
-    score += 1
-else:print("Incorrect :(")
+    print(f"Correct answer {name}:)")
+    score += 1  #incrementing the score as it's sees to be correct
+else:print(f"{name}, Incorrect :(")
 
 
 print()
 answer = input("What is next prime no. after 7? ")
 if answer == '11' or answer.lower() == "eleven":
-    print("Correct answer :)")
-    score += 1
-else:print("Incorrect :(")
+    print(f"Correct answer {name}:)")
+    score += 1  #incrementing the score as it's sees to be correct
+else:print(f"{name}, Incorrect :(")
 
 
 print()
 answer = input("How many months are in one year? ")
 if answer == '12' or answer.lower() == "twelve":
-    print("Correct answer :)")
-    score += 1
-else:print("Incorrect :(")
+    print(f"Correct answer {name}:)")
+    score += 1  #incrementing the score as it's sees to be correct
+else:print(f"{name}, Incorrect :(")
 
 
 print()
 answer = input("50 multiply of 8 is ")
 if answer == '400' or answer.lower() == "four hundred":
-    print("Correct answer :)")
-    score += 1
-else:print("Incorrect :(")
+    print(f"Correct answer {name}:)")
+    score += 1  #incrementing the score as it's sees to be correct
+else:print(f"{name}, Incorrect :(")
 
 
 print()
 answer = input("17 plus 15 = ")
 if answer == '32' or answer.lower() == "thirty two":
-    print("Correct answer :)")
-    score += 1
-else:print("Incorrect :(")
+    print(f"Correct answer {name}:)")
+    score += 1  #incrementing the score as it's sees to be correct
+else:print(f"{name}, Incorrect :(")
 
 print("\n(---------------hey game is over!!---------------)")
 
-print(f"\nyou got correct {score} out of 5 \nyou got " +str((score/5)*100) + "%.")
-
-print("\nKeep growing, keep hustling! :) ")
+#printing the score and percentage as the user corrected the answer
+print(f"\n{name}, you got correct {score} out of 5 \nyou got " +str((score/5)*100) + "%.")
+print(f"\n{name} Keep growing, keep hustling! :) ")
